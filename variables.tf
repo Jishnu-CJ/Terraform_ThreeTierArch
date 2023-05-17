6f684ad146cbef8b2950e56b35ccd50d43e2aa1f
Jishnu CJ <89075369+Jishnu-CJ@users.noreply.github.com>
2023-05-17 20:21:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -58,7 +58,7 @@ variable "private-db-subnet-2-cidr" {
 ## App tier security group ##
 ############################
 variable "ssh-locate" {
-  default     = "yourip"
+  default     = "35.93.47.131"
   description = "ip address"
   type        = string
 }
